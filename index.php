d0a44bec25c363579706fdeaf79bc5130936b9cb
Vinoth A M <vinoth.am2012@gmail.com>
2018-06-17 22:30:45 +0530

    Meta tag added
    Meta tag added in index.html file

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $result = $db_handle->runQuery($query);
 ?>
 
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css"></link>
 <style>
 li:hover {
